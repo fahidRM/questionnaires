25c2d2db25fcfcea8cde7dd8d5eea069fd439e43
Fahid Mohammed <fahid.rabiu@gmail.com>
2021-07-20 10:23:34 +0100

    minor fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,30 +10,44 @@ angular.module('app.pages', [])
 
             let vm =  this;
             vm.currentPage =  0;
+            vm.currentQuestion = 0;
             vm.data = {
                 demographics: {
                     age: "",
                     education: "",
                     gender: "",
                     occupation: "",
-                    prolific: ""
+                    prolific: "",
+                    use_of_visualisations : ""
                 },
                 responses: {
                     a: {
                         a: "",
+                        a_res: "",
                         b: "",
+                        b_res: "",
                         c: "",
+                        c_res: "",
                         d: "",
+                        d_res: "",
                         e: "",
-                        f: ""
+                        e_res: "",
+                        f: "",
+                        f_res: ""
                     },
                     b: {
                         a: "",
+                        a_res: "",
                         b: "",
+                        b_res: "",
                         c: "",
+                        c_res: "",
                         d: "",
+                        d_res: "",
                         e: "",
-                        f: ""
+                        e_res: "",
+                        f: "",
+                        f_res: ""
                     },
                 }
             };
@@ -54,16 +68,41 @@ angular.module('app.pages', [])
                     messagingSenderId: "75203192387",
                     appId: "1:75203192387:web:ae9913848a32864c9596f6"
                 });
-                fs =  firebase.firestore().collection('q2d1');
-                fc =  firebase.firestore().collection('q2dc');
+                fs =  firebase.firestore().collection('july_21');
+                fc =  firebase.firestore().collection('xpxpxp');
             }
 
             vm.gotoNextPage = function () {
                 vm.currentPage ++;
+                vm.currentQuestion = 0;
             }
 
             vm.gotoPrevPage = function () {
                 vm.currentPage --;
+                vm.currentQuestion = 0;
+            }
+
+            vm.gotoNextQuestion = function () {
+                vm.currentQuestion ++;
+                if (vm.currentPage == 2) {
+                    if (vm.currentQuestion >= 6) {
+                        vm.gotoNextPage();
+                    }
+                } else if (vm.currentPage == 3) {
+                    if (vm.currentQuestion >= 7) {
+                        alert("here");
+                        //vm.gotoNextPage();
+                    }
+                }
+            }
+
+            vm.gotoPrevQuestion = function () {
+                vm.currentQuestion --;
+                if (vm.currentPage == 2 || vm.currentPage == 3) {
+                    if (vm.currentQuestion <= 0) {
+                        vm.gotoPrevPage();
+                    }
+                }
             }
 
             vm.submit =  function () {
